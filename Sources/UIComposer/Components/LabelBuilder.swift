aebc06c4a03342ad4aaee1aa48448c654c8a242d
Bemol <110437038+bitbemol@users.noreply.github.com>
2024-06-04 19:33:14 -0600

    added simple label builder

---
+++ Sources/UIComposer/Components/LabelBuilder.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+enum LabelStyle {
+    case title
+    case subtitle
+    case body
+    case caption
+}
+
+
+final class LabelBuilder {
+    private var label = UILabel()
+    
+    func withTAMIC(_ activate: Bool) -> Self {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return self
+    }
+    
+    func withText(_ text: String) -> Self {
+        label.text = text
+        return self
+    }
+    
+    func withStyle(_ labelStyle: LabelStyle) -> Self {
+        switch labelStyle {
+        case .title:
+            label.font = .boldSystemFont(ofSize: 24)
+            label.textColor = .black
+            label.backgroundColor = .clear
+        case .subtitle:
+            label.font = .systemFont(ofSize: 18)
+            label.textColor = .darkGray
+            label.backgroundColor = .clear
+        case .body:
+            label.font = .systemFont(ofSize: 16)
+            label.textColor = .gray
+            label.backgroundColor = .clear
+        case .caption:
+            label.font = .systemFont(ofSize: 12)
+            label.textColor = .lightGray
+            label.backgroundColor = .clear
+        }
+        return self
+    }
+    
+    func build() -> UILabel {
+        label
+    }
+}
---
+++ Tests/UIComposerTests/LabelBuilderTests.swift
@@ -0,0 +1,67 @@
+@testable import UIComposer
+import XCTest
+
+final class LabelBuilderTests: XCTestCase {
+
+    func testLabelBuilder() {
+            let label = LabelBuilder()
+                .withTAMIC(false)
+                .withText("Hello World")
+                .build()
+            
+            XCTAssertEqual(label.text, "Hello World")
+        }
+        
+        func testTitleLabelStyle() {
+            let label = LabelBuilder()
+                .withTAMIC(false)
+                .withText("Title Label")
+                .withStyle(.title)
+                .build()
+            
+            XCTAssertEqual(label.text, "Title Label")
+            XCTAssertEqual(label.font, UIFont.boldSystemFont(ofSize: 24))
+            XCTAssertEqual(label.textColor, UIColor.black)
+            XCTAssertEqual(label.backgroundColor, UIColor.clear)
+        }
+        
+        func testSubtitleLabelStyle() {
+            let label = LabelBuilder()
+                .withTAMIC(false)
+                .withText("Subtitle Label")
+                .withStyle(.subtitle)
+                .build()
+            
+            XCTAssertEqual(label.text, "Subtitle Label")
+            XCTAssertEqual(label.font, UIFont.systemFont(ofSize: 18))
+            XCTAssertEqual(label.textColor, UIColor.darkGray)
+            XCTAssertEqual(label.backgroundColor, UIColor.clear)
+        }
+        
+        func testBodyLabelStyle() {
+            let label = LabelBuilder()
+                .withTAMIC(false)
+                .withText("Body Label")
+                .withStyle(.body)
+                .build()
+            
+            XCTAssertEqual(label.text, "Body Label")
+            XCTAssertEqual(label.font, UIFont.systemFont(ofSize: 16))
+            XCTAssertEqual(label.textColor, UIColor.gray)
+            XCTAssertEqual(label.backgroundColor, UIColor.clear)
+        }
+        
+        func testCaptionLabelStyle() {
+            let label = LabelBuilder()
+                .withTAMIC(false)
+                .withText("Caption Label")
+                .withStyle(.caption)
+                .build()
+            
+            XCTAssertEqual(label.text, "Caption Label")
+            XCTAssertEqual(label.font, UIFont.systemFont(ofSize: 12))
+            XCTAssertEqual(label.textColor, UIColor.lightGray)
+            XCTAssertEqual(label.backgroundColor, UIColor.clear)
+        }
+
+}
