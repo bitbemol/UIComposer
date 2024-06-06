fe7f5c005b93beee7358281208b5cb345ef1986e
Bemol <110437038+bitbemol@users.noreply.github.com>
2024-06-06 00:48:08 -0600

    added padding properties

--- Sources/UIComposer/Constraints/SimpleConstraints.swift
+++ Sources/UIComposer/Constraints/SimpleConstraints.swift
@@ -1,8 +1,13 @@
 import UIKit
 
+public enum Padding {
+    case vertical(top: CGFloat, bottom: CGFloat)
+    case horizontal(leading: CGFloat, trailing: CGFloat)
+}
+
 public struct SimpleConstraints {
     /// Set the new view to all sides of the super view
-    static public func stickToAllSides(for newView: UIView, to view: UIView, with padding: CGFloat = 0) -> [NSLayoutConstraint] {
+    public func stickToAllSides(for newView: UIView, to view: UIView, with padding: CGFloat = 0) -> [NSLayoutConstraint] {
         [
             newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
             newView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
@@ -12,27 +17,43 @@ public struct SimpleConstraints {
     }
     
     /// Set subsequent views to the given view, use superView to stick the new view to the horizontal of the super view
-    static public func stickTop(of newView: UIView, to view: UIView, with padding: CGFloat = 0, superView: UIView) -> [NSLayoutConstraint]  {
-        [
-            newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: padding),
-            newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: padding),
-            newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+    public func stickTop(of newView: UIView, to view: UIView, with separation: CGFloat = 0, superView: UIView, padding: Padding?) -> [NSLayoutConstraint]  {
+        if case let .horizontal(leading, trailing) = padding {
+            return [
+                newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: separation),
+                newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: leading),
+                newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: -trailing),
+                
+            ]
+        }
+        return [
+            newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: separation),
+            newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor),
+            newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor),
             
         ]
     }
     
     /// Set the first view to the super view
-    static public func stickTop(of newView: UIView, to superView: UIView, with padding: CGFloat = 0) -> [NSLayoutConstraint]  {
-        [
-            newView.topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: padding),
-            newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: padding),
-            newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+    public func stickTop(of newView: UIView, to superView: UIView, with separation: CGFloat = 0, padding: Padding?) -> [NSLayoutConstraint]  {
+        if case let .horizontal(leading, trailing) = padding {
+            return [
+                newView.topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: separation),
+                newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: leading),
+                newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: -trailing),
+                
+            ]
+        }
+        return [
+            newView.topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: separation),
+            newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor),
+            newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor),
             
         ]
     }
     
     /// Set the given view to the super view center
-    static public func stickToCenter(of superView: UIView, view: UIView) -> [NSLayoutConstraint] {
+    public func stickToCenter(of superView: UIView, view: UIView) -> [NSLayoutConstraint] {
         [
             view.centerXAnchor.constraint(equalTo: superView.centerXAnchor),
             view.centerYAnchor.constraint(equalTo: superView.centerYAnchor)
