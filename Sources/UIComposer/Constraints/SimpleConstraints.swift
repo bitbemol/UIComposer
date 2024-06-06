7fd6e6a5d03b5a4aa368554f250c87d98e314cfa
Bemol <110437038+bitbemol@users.noreply.github.com>
2024-06-06 01:13:18 -0600

    simplify parameters

--- Sources/UIComposer/Constraints/SimpleConstraints.swift
+++ Sources/UIComposer/Constraints/SimpleConstraints.swift
@@ -5,6 +5,10 @@ public enum Padding {
     case horizontal(leading: CGFloat, trailing: CGFloat)
 }
 
+enum SimpleConstraintsError: Error {
+    case missingSuperView
+}
+
 public struct SimpleConstraints {
     
     public init() { }
@@ -19,47 +23,59 @@ public struct SimpleConstraints {
         ]
     }
     
-    /// Set subsequent views to the given view, use superView to stick the new view to the horizontal of the super view
-    public func stickTop(of newView: UIView, to view: UIView, with separation: CGFloat = 0, superView: UIView, padding: Padding?) -> [NSLayoutConstraint]  {
+    /// Set subsequent views to the given view, use superView to stick the new view to the horizontal of the super view.  If no superview is set then this will throws an error.
+    public func stickTop(of newView: UIView, to view: UIView, with separation: CGFloat = 0, padding: Padding?) throws -> [NSLayoutConstraint]  {
+        guard let superview = newView.superview else { throw SimpleConstraintsError.missingSuperView }
         if case let .horizontal(leading, trailing) = padding {
             return [
                 newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: separation),
-                newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: leading),
-                newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: -trailing),
+                newView.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor, constant: leading),
+                newView.trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor, constant: -trailing),
                 
             ]
         }
         return [
             newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: separation),
-            newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor),
-            newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor),
+            newView.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor),
+            newView.trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor),
             
         ]
     }
     
-    /// Set the first view to the super view
-    public func stickTop(of newView: UIView, to superView: UIView, with separation: CGFloat = 0, padding: Padding?) -> [NSLayoutConstraint]  {
+    /// Set the first view to the super view.  If no superview is set then this will throws an error.
+    public func stickTop(of newView: UIView, with separation: CGFloat = 0, padding: Padding?) throws -> [NSLayoutConstraint]  {
+        guard let superview = newView.superview else { throw SimpleConstraintsError.missingSuperView }
         if case let .horizontal(leading, trailing) = padding {
             return [
-                newView.topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor, constant: separation),
-                newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: leading),
-                newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: -trailing),
+                newView.topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor, constant: separation),
+                newView.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor, constant: leading),
+                newView.trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor, constant: -trailing),
                 
             ]
         }
         return [
-            newView.topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: separation),
-            newView.leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor),
-            newView.trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor),
+            newView.topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.bottomAnchor, constant: separation),
+            newView.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor),
+            newView.trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor),
             
         ]
     }
     
-    /// Set the given view to the super view center
-    public func stickToCenter(of superView: UIView, view: UIView) -> [NSLayoutConstraint] {
-        [
-            view.centerXAnchor.constraint(equalTo: superView.centerXAnchor),
-            view.centerYAnchor.constraint(equalTo: superView.centerYAnchor)
+    /// Set subsequent views to the given view, centering the new view to the x center. If no superview is set then this will throws an error.
+    public func stickCentrallyTop(of newView: UIView, to view: UIView, with separation: CGFloat = 0) throws -> [NSLayoutConstraint] {
+        guard let superview = newView.superview else { throw SimpleConstraintsError.missingSuperView }
+        return [
+            newView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: separation),
+            newView.centerXAnchor.constraint(equalTo: superview.centerXAnchor)
+        ]
+    }
+    
+    /// Set the given view to the super view center. If no superview is set then this will throws an error.
+    public func stickToCenter(_ view: UIView) throws -> [NSLayoutConstraint] {
+        guard let superview = view.superview else { throw SimpleConstraintsError.missingSuperView }
+        return [
+            view.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            view.centerYAnchor.constraint(equalTo: superview.centerYAnchor)
         ]
     }
 }
