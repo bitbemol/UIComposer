918471ff29e7248e71a0f4389ebad96ee38298e2
Bemol <110437038+bitbemol@users.noreply.github.com>
2024-06-05 23:24:23 -0600

    Simple image builder

---
+++ Sources/UIComposer/Components/ImageBuilder.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+public final class ImageBuilder {
+    private var image = UIImageView()
+    
+    public func withAutoLayout() -> Self {
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return self
+    }
+    
+    public func withContentMode(_ mode: UIView.ContentMode) -> Self {
+        image.contentMode = mode
+        return self
+    }
+    
+    public func withClipToBound(_ activate: Bool) -> Self {
+        image.clipsToBounds = activate
+        return self
+    }
+    
+    public func withImage(_ imageName: String) -> Self {
+        image.image = UIImage(named: imageName)
+        return self
+    }
+    
+    public func build() -> UIImageView {
+        image
+    }
+}
